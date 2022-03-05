55db36c79103e6aac79110aeebfbf8fcbd22a0d6
Brittany Garrett <brittanymgarrett@gmail.com>
2022-03-04 20:16:17 -0600

    App running

---
+++ src/components/Goalizer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Goalizer.css"
+
+export const Goalizer = () => (
+    <>
+       <h2>Goalizer 🎯</h2>
+        <small>The goal organizer!</small>
+        <span>
+            <div>Coming soon!</div>
+        </span>
+    </>
+)
