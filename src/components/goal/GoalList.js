641ae8a21bc4ce12fd406f3e577d38b4286af38d
Brittany Garrett <brittanymgarrett@gmail.com>
2022-03-20 04:31:11 -0500

    Updated some goal list view

--- src/components/goal/GoalList.js
+++ src/components/goal/GoalList.js
@@ -23,14 +23,33 @@ export const GoalList = () => {
               setShowGoalForm(false);
             }
           }}
-          >
-          {goal.is_complete && <p style={{color: 'gold', fontSize: '25px', border:'none', background: 'none', cursor: 'pointer'}}>🎯</p>}
-          {goal.is_favorite && <p style={{color: 'gold', fontSize: '25px', border:'none', background: 'none', cursor: 'pointer'}}>⭐️</p>}
+          ><div className="goalWrapper" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', padding: '10px 10px', alignItems: 'center'
+        }}>
+            <div className="goalDateWrapper" style={{ display: 'flex', flexDirection: 'column'}}>
+              <p className="goalMonth" style={{margin: '0px', fontSize: '30px'}}>Mar</p>  
+              <p className="goalDay" style={{margin: '0px', fontSize: '30px'}}>25</p>  
+            </div>
+            <div className="goalDetailWrapper" style={{ width: '100%'}}>
+              <div className="goalTitleBold" style={{fontSize: '16px', fontWeight: 'bold', display: 'flex', flexWrap: 'wrap'}}>
+              {/* {goal?.title} */}
+              Title Here
+              </div>  
+              <div className="goalDescriptionInfo" style={{ marginBottom: '10px', marginTop: '10px' }}>
+                Details here...
+              </div>  
+              <div className="goalCompleteDiv" style={{display: 'flex', justifyContent: 'flex-end', marginTop: '-18px' }}>
+                <div className="goalDone" style={{fontSize: '20px', right: '10px'}}>🎯</div>
+              </div>  
+            </div>
+          </div>
+
           <p>{goal?.title}</p>
           <p>{goal?.description}</p>
           <p>{shortenedDate(goal)}</p>
           <p>{goal?.is_complete ? "Is complete" : "Is not complete"}</p>
           <p>{goal?.is_favorite ? "Is favorite" : "Is not favorite"}</p>
+          {goal.is_complete && <p style={{color: 'gold', fontSize: '25px', border:'none', background: 'none', cursor: 'pointer'}}>🎯</p>}
+          {goal.is_favorite && <p style={{color: 'gold', fontSize: '25px', border:'none', background: 'none', cursor: 'pointer'}}>⭐️</p>}
 
         </button>
       );
@@ -46,7 +65,6 @@ export const GoalList = () => {
 
   return (
     <>
-      <h1>Goals List</h1>
       { goalsForFolder.length > 0 ? (
         goalsMapped()
       ) : (
